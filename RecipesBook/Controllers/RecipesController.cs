1786390f390818fe675b8a5f2f3e6067bf783ea2
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-23 14:03:57 -0700

    create index route for recipes

---
+++ RecipesBook/Controllers/RecipesController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using RecipeBook.Models;
+
+namespace RecipeBook.Controllers
+{
+  [Authorize]
+  public class RecipesController : Controller
+  {
+    private readonly RecipeBookContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public RecipesController(UserManager<ApplicationUser> userManager, RecipeBookContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userRecipes = _db.Recipes.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userRecipes);
+    }
+  }
+}
