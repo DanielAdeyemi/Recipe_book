e342f7b866516275fd6344b3673f2f43526d72b2
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-24 10:04:38 -0700

    add tags controller with index route

---
+++ RecipesBook/Controllers/TagsController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using RecipeBook.Models;
+
+namespace RecipeBook.Controllers
+{
+  [Authorize]
+  public class TagsController : Controller
+  {
+    private readonly RecipeBookContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public TagsController(UserManager<ApplicationUser> userManager, RecipeBookContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userTags = _db.Tags.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userTags);
+    }
+  }
+}
